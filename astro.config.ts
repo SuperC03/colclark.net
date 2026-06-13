93f3781f589bc2fff8e344fd6fe5c193bf020f21
Colin C <contact@colclark.net>
2026-06-13 17:38:42 -0400

    fixed canonical url error

--- astro.config.ts
+++ astro.config.ts
@@ -8,6 +8,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://colclark.net",
+  trailingSlash: "never",
   adapter: cloudflare(),
   integrations: [markdoc(), sitemap()],
   session: {
